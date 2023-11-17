40db022885c26590662cd411506ced05c8998c05
Jaden Arceneaux <arceneauxjaden@gmail.com>
2023-11-16 22:07:17 -0800

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ impl NodeValue {
     fn to_i32(&self) -> i32 {
         match self {
             NodeValue::Value(val) => val.clone(),
-            _ => 1
+            _ => -1
         }
     }
 }
@@ -79,16 +79,16 @@ impl Node {
                 next.get_recursive(target, cur + 1)
             }
             Address::Nil => {
-                return Err(format!("Index {} not found", target))
+                return Err(format!("Index {} not found", target-1))
             }
         }
     }
 
     fn get(&self, index: usize) -> Result<i32, String> {
-        self.get_recursive(index, 0)
+        self.get_recursive(index + 1, 0)
     }
 
-    fn remove_recursive(&mut self, target: usize, cur: usize) -> i32 {
+    fn remove_recursive(&mut self, target: usize, cur: usize) -> Result<i32, String> {
         if target - 1 == cur {
             return match self.next {
                 Address::Address(ref next) => {
@@ -96,12 +96,14 @@ impl Node {
                         Address::Address(ref next) => {
                             next.value.to_i32()
                         }
-                        Address::Nil => -1
+                        Address::Nil => {
+                            return Err(format!("Index {} not found", target-1))
+                        }
                     };
                     self.next = next.next.to_owned();
-                    return val;
+                    Ok(val)
                 }
-                Address::Nil => -1
+                Address::Nil => Err(format!("Index {} not found", target-1))
 
             }
         }
@@ -109,17 +111,16 @@ impl Node {
             Address::Address(ref mut next) => {
                 next.remove_recursive(target, cur + 1)
             }
-            Address::Nil => -1
+            Address::Nil => Err(format!("Index {} not found", target-1))
         }
     }
 
-    fn remove(&mut self, index: usize) -> i32 {
-        self.remove_recursive(index, 0)
+    fn remove(&mut self, index: usize) -> Result<i32, String> {
+        self.remove_recursive(index + 1, 0)
     }
 
 
 }
-
 
 fn main() {
     let mut n = Node::new();
@@ -127,7 +128,5 @@ fn main() {
         n.append(i);
     }
     println!("{}", n.to_string());
-    println!("{}", n.remove(2));
-    println!("{}", n.to_string());
-    println!("{}", n.get(5).expect("couldn't find"));
+    println!("{}", n.get(9).expect("couldn't find"));
 }
